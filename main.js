c5b3f9e3f7c4bbb393feebcc0008bd102a1fa132
RAUNAK BANJARI <88834316+raunakbanjari@users.noreply.github.com>
2022-06-07 16:50:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+noseX = 0;
+noseY = 0;
+rightWristX = 0;
+leftWristX = 0;
+difference = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(500,500);
 
     canvas = createCanvas(500,400);
-    canvas.position(650,110);
+    canvas.position(650,140);
 
     posenet = ml5.poseNet(video,modelLoaded);
     posenet.on('pose', gotPoses);
@@ -11,6 +17,11 @@ function setup(){
 
 function draw(){
     background('#05f7e3');
+
+    document.getElementById("square_side").innerHTML = "The size of the square is " + difference;
+    fill('#9c0202');
+    stroke('#e607da');
+    square(noseX , noseY , difference);
 }
 
 function modelLoaded(){
@@ -19,5 +30,12 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose X = " + noseX + " and nose Y = " + noseY);
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        difference = floor(leftWristX-rightWristX);
+        console.log("Left wrist X = " + leftWristX + " and right wrist X = " + rightWristX);
     }
 }
